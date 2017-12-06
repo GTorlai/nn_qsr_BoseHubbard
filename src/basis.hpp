e556c0bf944f02fd94f03264a1ea8cc33cf8b2cc
gtorlai <gtorlai@uwaterloo.ca>
2017-12-05 19:08:20 -0800

    Basis with restricted site occupancy - tested

--- src/basis.hpp
+++ src/basis.hpp
@@ -11,11 +11,13 @@ class Basis{
     int nsites_;
     //Number of bosons
     int M_;
+    //Maximum bosons occupaty at any site
+    int M_max_;
     //Number of binary variables
     int nv_;
     //Dimension of the Hilbert space
     unsigned long long D_;
-    
+
 public:
     
     //Basis states in occupation number basis
@@ -23,18 +25,14 @@ public:
     //Basis state in binary form
     Eigen::MatrixXd states_bin_;
     
-    Basis(int nsites,int M):nsites_(nsites),M_(M){
-        if (nsites_>10){
-            std::cout<<"Dimension of the Hilbert space too large!"<<std::endl;
-        }
-        else {
-            D_ = tools::Factorial(M_+nsites_-1)/(tools::Factorial(M_)*tools::Factorial(nsites_-1));
-            nv_ = (M_+1)*nsites_;
-            states_mul_.resize(D_,nsites_);
-            states_bin_.resize(D_,nv_);
-            GenerateBasis();
-        }   
+    Basis(int nsites,int M,int M_max):nsites_(nsites),M_(M),M_max_(M_max){
+        GetDimension();
+        nv_ = (M_max+1)*nsites_;
+        states_mul_.resize(D_,nsites_);
+        states_bin_.resize(D_,nv_);
+        GenerateBasis();
     }
+    
     inline int Nsites(){
         return nsites_;
     }
@@ -45,8 +43,54 @@ public:
         return D_;
     }
 
+    void GetDimension(){
+        //Full basis
+        if (M_ == M_max_){
+            D_ = tools::Factorial(M_+nsites_-1)/(tools::Factorial(M_)*tools::Factorial(nsites_-1));
+            //std::cout << "Dimension = " << D_ << std::endl;
+        }
+        //Restriced basis
+        else {
+            D_= GetNumCoefficient(M_max_,M_,nsites_);
+            //std::cout << "Dimension = " << D_ << std::endl;
+        }
+    }
+
     //Generate the basis (multinomial and binomial)
     void GenerateBasis() {
+        //Full basis
+        if (M_ == M_max_){
+            FullBasis();
+        }
+        //Restriced basis
+        else {
+            RestrictedBasis();
+        }
+     
+    }
+
+    //Get coefficient for restricted basis
+    int GetNumCoefficient(int M_max, int M, int nsites){
+        int C = 0;
+        if ((M==0) && (nsites == 0)){
+            return 1;
+        }
+        if (M<0) {
+            return 0;
+        }
+        if (M_max*nsites < M){
+            return 0;
+        }
+        else { 
+            for(int L=M-M_max;L<M+1;L++){
+                C += GetNumCoefficient(M_max,L,nsites-1);
+            }
+        }
+        return C;
+    }
+
+    //Generate the basis (multinomial and binomial)
+    void FullBasis() {
         Eigen::VectorXd boson_number(nsites_);
         Eigen::VectorXd state(nv_);
         
@@ -60,8 +104,6 @@ public:
             if (boson_number(0)>0){
                 boson_number(0)--;
                 boson_number(1)++;
-                state_counter++;
-                states_mul_.row(state_counter) = boson_number;
             }
             else {
                 int index=0;
@@ -78,14 +120,79 @@ public:
                 boson_number(0) = boson_number(index) -1;  
                 boson_number(index) = 0;
                 boson_number(index+1)++;
-                state_counter++;
-                states_mul_.row(state_counter) = boson_number;
             }
+            state_counter++;
+            states_mul_.row(state_counter) = boson_number;
         }
         for(int i=0;i<D_;i++){
             tools::MultinomialToBinomial(nsites_,M_,states_mul_.row(i),state);
             states_bin_.row(i) = state;
-            //std::cout<<states_bin_.row(i)<<std::endl;
+        }
+    }
+
+    //Generate the basis (multinomial and binomial)
+    void RestrictedBasis() {
+        Eigen::VectorXd boson_number(nsites_);
+        Eigen::VectorXd state(nv_);
+        
+        boson_number.setZero();
+        states_mul_.setZero();
+        int M_div_Mmax = std::floor(double(M_)/double(M_max_));
+        for (int i=0;i<M_div_Mmax;i++){
+            boson_number(i) = M_max_;
+        }
+        boson_number(M_div_Mmax) = M_-M_max_*M_div_Mmax;
+        states_mul_.row(0) = boson_number;
+        bool exit = false;
+        int state_counter=0;
+        int delta;
+        for(int s=1;s<D_;s++){
+            if (boson_number(0)>0){
+                delta = 0;
+                if (boson_number(0) < M_max_){
+                    delta = M_max_-boson_number(0);
+                    boson_number(0) = M_max_;
+                }
+                int index;
+                for (int i=1;i<nsites_;i++){
+                    if((boson_number(i)<M_max_)){
+                        index = i;
+                        break;
+                    }
+                }
+                boson_number(index)++;
+                boson_number(index-1) -= 1 + delta;
+            }
+            else {
+                int index_j;
+                int index_k;
+                for (int j=1;j<nsites_;j++){
+                    if((boson_number(j)>0)){
+                        index_j = j;
+                        break;
+                    }
+                }
+                for (int i=index_j+1;i<nsites_;i++){
+                    if((boson_number(i)<M_max_)){
+                        index_k=i;
+                        break;
+                    }
+                }
+                boson_number(index_k)++;
+                boson_number(index_k-index_j-1) = boson_number(index_j)-1;
+                for (int l=0;l<index_k-index_j-1;l++){
+                    boson_number(l) = M_max_;
+                }
+                for(int l=index_k-index_j;l<index_k;l++){
+                    boson_number(l)= 0;
+                }
+            }
+            state_counter++;
+            states_mul_.row(state_counter) = boson_number;
+        }
+        for(int i=0;i<D_;i++){
+            tools::MultinomialToBinomial(nsites_,M_max_,states_mul_.row(i),state);
+            states_bin_.row(i) = state;
         }
     }
     
@@ -99,7 +206,6 @@ public:
             std::cout<<"\tBinomial =  "<<states_bin_.row(i)<<std::endl;
         }
     }
- 
 };
 }
 
